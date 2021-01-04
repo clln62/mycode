fa3ceece58c8fdcc6f7a23c41a147337ae1e6c2c
Collin Fairlie <collin.fairlie@gmail.com>
2021-01-04 18:48:53 +0000

    Shebang Challenge complete.

---
+++ lab_input/shebang_challenge.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+name_input = input("Please enter your name:")
+day_input = input("Please enter the day of the week:")
+
+print("Hello,", name_input + "!", "Happy", day_input + "!")

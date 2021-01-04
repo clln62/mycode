7f6e0d38e87574996a5afb56e8b8c05408d5543b
Collin Fairlie <collin.fairlie@gmail.com>
2021-01-04 20:37:08 +0000

    Lab 8

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -10,4 +10,4 @@ print("The last item in the list (state): " + my_list[2] )
 
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
 
-print(f"When I ssh into IP addresses 10.0.0.1 or 10.20.30.1 I am unable to ping ports 5060, 80, or 55.")
+print(f"When I {new_list[5]} into IP addresses {new_list[3]} or {new_list[4]} I am unable to ping ports {new_list[0]}, {new_list[1]}, or  {new_list[2]}.")
